140a512c42b1ca672cc721198e1df7b79f37ef63
rajnish kumar <rajnishkk97@gmail.com>
2025-08-09 01:18:40 +0530

    fix: Instance

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // Create instance with base URL
 const axiosInstance = axios.create({
-  baseURL:  process.env.REACT_APP_API_BASE_URL||'http://localhost:5000/api',
+  baseURL:  process.env.REACT_APP_API_BASE_URL,
   withCredentials: true, // Sends httpOnly refresh cookies
 });
 
