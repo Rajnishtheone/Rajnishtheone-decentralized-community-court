6127c9ba37449ae1f64af5f8bc03d2e3edfa9522
Rajnish Kumar <rajnishkk97@gmail.com>
2026-02-12 16:33:44 +0530

    chore(frontend): add eslint config

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/no-unescaped-entities': 'off',
+        'no-unused-vars': 'off',
+    },
+};
